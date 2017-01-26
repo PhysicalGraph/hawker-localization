f026380180f266d7501b41dee732e127c9523240
Andrew Bresee <andrew.bresee@smartthings.com>
2017-01-25 16:38:20 -0800

    Update convertExcel, and include contentful-management to allow for parsing of excel, and upload to Contentful
    Update app to crawl excel sheets more effectively, upload locales in batches, and avoid API rate limit by using setTimeout
    
    Handle right justified right justified text for locales that read right to left

--- js/uploadToContentful.js
+++ js/uploadToContentful.js
@@ -1,38 +1,37 @@
-// Documentation for uploading found here:
-// https://www.contentful.com/developers/docs/references/content-management-api/#/introduction/resource-ids
-
-// get a single entry:
-// https://api.contentful.com/spaces/space_id/entries/entry_id
-
-// PUBLISH an entry PUT:
-// https://api.contentful.com/spaces/space_id/entries/entry_id
-// include in the POST request:
-// Fields Object
-//// Title Object
-////// en-us: string
-//// Body Object
-////// en-us: string
-
-import rp from 'request-promise';
 import config from  '~/config.json';
-import request from 'request'
+import contentful from 'contentful-management';
 
-export function uploadToContentful(entryId, updatedEntry) {
-  console.log('uploadToContentful called!')
-  let queryString = 'https://api.contentful.com/spaces/'+config.contentfulSpace+'/entries/'+'entryId'+'/?access_token='+config.contentfulManagementToken;
-  console.log('queryString: ', queryString);
-  // initially GET the entry
-  let getItem = rp(queryString);
-  getItem.then(function(entireBody) {
-    // THEN update the entry. Send back to Contentful the entireBody + updated information
-    // Will need to update this to send back an updated body
-    request.post({url:queryString}, function (e, r, body) {
-      if (e) {
-        console.log(e)
-      } else {
-        console.log(body)
-      }
-    })
-    console.log('entireBody returned as: ', entireBody)
+
+export function uploadToContentful(deviceInfoToUpdate) {
+
+  var client = contentful.createClient({
+    accessToken: config.contentfulManagementToken
+  })
+
+
+  client.getSpace(config.contentfulSpace)
+  .then((space) => {
+    setTimeout(() => {
+      space.getEntry(deviceInfoToUpdate.deviceEntryID)
+      .then((entry) => {
+        for (let locale in deviceInfoToUpdate) {
+          if (locale != 'deviceEntryID') {
+            let currentLocale = deviceInfoToUpdate[locale]
+            let currentLocaleCode = currentLocale.locale.replace(/_/g, "-")
+            let translationMessage = currentLocale.translation;
+            let entryID = currentLocale.entryID;
+            let fieldToUpdate = currentLocale.fieldID;
+            entry.fields[fieldToUpdate][currentLocaleCode] = translationMessage;
+          }
+        }
+        entry.update()
+        .then((updatedContentType, err) => {
+          if (err) {
+            console.log("There was an error!!: ", err);
+          }
+          console.log('Update was successful')
+        })
+      })
+    }, 200)
   })
 }
