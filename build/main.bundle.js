a100e1fba603b8f99cc4957ddedc0510c64de697
Andrew Bresee <andrew.bresee@smartthings.com>
2017-02-14 10:25:46 -0800

    Handle edge cases including rate limiting issues. - Get the space before begining upload - Create a new field on an entry that does not have it - Handle errors properly when they occur

--- build/main.bundle.js
+++ build/main.bundle.js
@@ -8,14 +8,11 @@ var _convertExcelToContentfulObject = require('../js/convertExcelToContentfulObj
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-var pathToFile1 = 'excelDocs/Turkish\ -with\ SL\ mapping\ keyword.xlsx';
-var pathToFile2 = 'excelDocs/testSheet.xlsx';
-var pathToFile3 = 'excelDocs/completeTestSheet.xlsx';
-var pathToFile4 = 'excelDocs/testTestLocaleCodes.xlsx';
+var pathToFile = 'excelDocs/finalFinal.xlsx';
 
 
 var readExcel = function readExcel() {
-  var workbook = _xlsx2.default.readFile(pathToFile4);
+  var workbook = _xlsx2.default.readFile(pathToFile);
   (0, _convertExcelToContentfulObject.convertExcelToContentfulObject)(workbook);
 };
 
--- js/uploadToContentful.js
+++ js/uploadToContentful.js
@@ -1,26 +1,22 @@
 import config from  '~/config.json';
-import contentful from 'contentful-management';
 
+export function uploadToContentful(deviceInfoToUpdate, uploadSpace, rowIndex) {
+  let waitTime = rowIndex * 100;
 
-export function uploadToContentful(deviceInfoToUpdate) {
-
-  var client = contentful.createClient({
-    accessToken: config.uploadToContentfulManagementToken
-  })
-
-
-  client.getSpace(config.uploadToContentfulSpace)
-  .then((space) => {
+  if (deviceInfoToUpdate.deviceEntryID != undefined) {
     setTimeout(() => {
-      space.getEntry(deviceInfoToUpdate.deviceEntryID)
+      uploadSpace.getEntry(deviceInfoToUpdate.deviceEntryID)
       .then((entry) => {
         for (let locale in deviceInfoToUpdate) {
-          if (locale != 'deviceEntryID') {
+          if (locale.toString() != 'deviceEntryID') {
             let currentLocale = deviceInfoToUpdate[locale]
-            let currentLocaleCode = currentLocale.locale.replace(/_/g, "-")
+            let currentLocaleCode = currentLocale.locale.replace(/_/g, "-").toString();
             let translationMessage = currentLocale.translation;
             let entryID = currentLocale.entryID;
-            let fieldToUpdate = currentLocale.fieldID;
+            let fieldToUpdate = currentLocale["fieldID"].toString();
+            if (entry.fields[fieldToUpdate] === undefined) {
+              entry.fields[fieldToUpdate] = {};
+            }
             entry.fields[fieldToUpdate][currentLocaleCode] = translationMessage;
           }
         }
@@ -28,7 +24,20 @@ export function uploadToContentful(deviceInfoToUpdate) {
         .then((updatedContentType) => {
           console.log('Update was successful')
         })
+        .catch((err) => {
+          console.log('\n ############### UPDATING entry ERROR: ', err)
+          console.log('\n ############### entry: ', entry)
+        })
+      }).
+      catch((err) => {
+        if (err.message = "The resource could not be found") {
+          console.log('\n ****************** Could not find device with ID: ', deviceInfoToUpdate.deviceEntryID)
+          console.log('\n ****************** There was an err in getting an item! for deviceInfoToUpdate: ', err)
+        } else {
+          console.log('\n ****************** There was an err in getting an item! for deviceInfoToUpdate: ', err)
+          console.log('\n ****************** deviceInfoToUpdate: ', JSON.stringify(deviceInfoToUpdate))
+        }
       })
-    }, 200)
-  })
+    }, waitTime)
+  }
 }
